bd7a0f980b8c7d25e879b4103963d4e2686cd7fb
matias maizares <matiasmaizares2017@gmail.com>
2024-10-29 18:19:53 -0300

    creacion del proyecto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './views/home/home.component';
+import { ContactoComponent } from './views/contacto/contacto.component';
+import { NosotrosComponent } from './views/nosotros/nosotros.component';
+import { ServiciosComponent } from './views/servicios/servicios.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'contacto', component: ContactoComponent },
+  { path: 'nosotros', component: NosotrosComponent },
+  { path: 'servicios', component: ServiciosComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
